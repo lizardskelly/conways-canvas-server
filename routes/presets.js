dec54f683a28479eb44d736f4180b9a748856bd2
Liz Meeker <lizmeeker90@gmail.com>
2022-07-05 16:29:11 -0400

    added GET method for preset list

--- routes/presets.js
+++ routes/presets.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const fs = require('fs');
 const router = express.Router();
+const { v4: uuidv4 } = require('uuid');
+
+
+router.get('/', (_req, res) => {
+  const presetData = JSON.parse(fs.readFileSync('data/presets.json'));
+  res.json(presetData.map(canvas => {
+    return {
+      name: canvas.name,
+      id: uuidv4()
+    }
+  }))
+});
 
 module.exports = router;
